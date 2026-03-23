619a3607c60f56c68a74983a9d8e5df39c840d14
otiliavod <otilia.vod@gmail.com>
2026-03-23 12:57:29 +0200

    Resolved git issues

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
@@ -13,27 +5,26 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
 
-    app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    credentials: true,
+  });
 
-    app.enableCors({
-        origin: ['http://localhost:4200'],
-        credentials: true,
-    });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            transform: true,
-            forbidNonWhitelisted: true,
-        }),
-    );
+  app.enableShutdownHooks();
 
-    app.enableShutdownHooks();
-
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
->>>>>>> 18a6ce5 (Created project, schema, added docker and nest)
